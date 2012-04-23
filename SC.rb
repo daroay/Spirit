bc0854091b1b0ad78d10b14d6fffec937a0425f5
Manuel Aude Morales <dormam@gmail.com>
2012-04-22 23:22:26 -0500

    Se creo maquina virtual

--- SC.rb
+++ SC.rb
@@ -2,7 +2,7 @@ $:.unshift( File.dirname( __FILE__ ) )
 require 'SpiritLexer'
 require 'SpiritParser'
 
-input = ANTLR3::FileStream.new(File.open('Operations.sp'))
+input = ANTLR3::FileStream.new(File.open('Basic.sp'))
 lexer = Spirit::Lexer.new( input )
 source = ANTLR3::CommonTokenStream.new( lexer )
 parser = Spirit::Parser.new( source )
